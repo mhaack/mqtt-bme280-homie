61b008616c5f5b36790c12add112f579d2db95b3
Markus Haack <markus.haack@gmail.com>
2017-02-02 16:15:14 +0100

    Add Homie config for interval and temp offset

--- src/BME280Node.h
+++ src/BME280Node.h
@@ -9,40 +9,34 @@
 #ifndef SRC_BME280NODE_H_
 #define SRC_BME280NODE_H_
 
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
+#include <SPI.h>
+#include <Wire.h>
 
 #include <HomieNode.hpp>
 
-class BME280Node: public HomieNode {
+class BME280Node : public HomieNode {
 private:
-    unsigned long lastMeasurement;
+  unsigned long lastMeasurement;
 
-    Adafruit_BME280 bme;
+  Adafruit_BME280 bme;
 
 public:
-    BME280Node();
-    virtual void setup() override;
-    virtual void loop() override;
+  BME280Node();
+  virtual void setup() override;
+  virtual void loop() override;
 
-    float getHumidity() const {
-        return humidity;
-    }
+  float getHumidity() const { return humidity; }
 
-    float getTemperature() const {
-        return temperature;
-    }
+  float getTemperature() const { return temperature; }
 
-    float getPressure() const {
-        return pressure;
-    }
+  float getPressure() const { return pressure; }
 
 private:
-    float temperature = NAN;
-    float humidity = NAN;
-    float pressure = NAN;
+  float temperature = NAN;
+  float humidity = NAN;
+  float pressure = NAN;
 };
 
 #endif
