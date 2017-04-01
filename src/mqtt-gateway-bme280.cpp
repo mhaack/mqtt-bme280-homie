9171c578420541a36b6b9b0f8ad77764c8e1adb7
Markus Haack <markus.haack@gmail.com>
2017-04-01 14:17:20 +0200

    version 1.0 sensor only, without RF functions

--- src/mqtt-gateway-bme280.cpp
+++ src/mqtt-gateway-bme280.cpp
@@ -13,21 +13,14 @@
 
 #include "BME280Node.h"
 #include "DisplayNode.h"
-#include "RFReceiverNode.h"
-#include "RFTransmitterNode.h"
 #include "images.h"
 
 // init time client
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
 
-// init rc library
-RCSwitch mySwitch = RCSwitch();
-
 // init Homie nodes
 BME280Node bme280Node("bme280Outdoor");
-RFReceiverNode rfReceiverNode("rfRxOutdoor", mySwitch);
-RFTransmitterNode rfTransmitterNode("rfTxOutdoor", mySwitch);
 
 // init display & display node
 SSD1306Wire display(0x3c, D2, D1);
@@ -41,19 +34,12 @@ void drawHumidityFrame(OLEDDisplay *display, OLEDDisplayUiState *state,
                        int16_t x, int16_t y);
 void drawPressureFrame(OLEDDisplay *display, OLEDDisplayUiState *state,
                        int16_t x, int16_t y);
-void drawRFTransmitFrame(OLEDDisplay *display, OLEDDisplayUiState *state,
-                         int16_t x, int16_t y);
 
 void loopHandler() { timeClient.update(); }
 
 void setup() {
   Serial.begin(115200);
   Serial << endl << endl;
-
-  // init RF library
-  mySwitch.enableTransmit(15);    // RF Transmitter on pin D8
-  mySwitch.setRepeatTransmit(20); // increase transmit repeat
-  mySwitch.enableReceive(13);     // RF Receiver on pin D7
 
   Homie_setFirmware("mqtt-gateway-outdoor", "1.0.0");
   Homie.disableResetTrigger();
@@ -65,8 +51,7 @@ void setup() {
   displayNode.addFrame(drawTempFrame, 0);
   displayNode.addFrame(drawHumidityFrame, 1);
   displayNode.addFrame(drawPressureFrame, 2);
-  displayNode.addFrame(drawRFTransmitFrame, 3);
-  displayNode.setFrameCount(4);
+  displayNode.setFrameCount(3);
   displayNode.enableStatusFrame(true);
 
   Homie.setup();
@@ -111,17 +96,6 @@ void drawPressureFrame(OLEDDisplay *display, OLEDDisplayUiState *state,
   display->setTextAlignment(TEXT_ALIGN_LEFT);
   display->drawString(x + 50, y + 25,
                       String(bme280Node.getPressure(), 0) + " hPa");
-}
-
-void drawRFTransmitFrame(OLEDDisplay *display, OLEDDisplayUiState *state,
-                         int16_t x, int16_t y) {
-  display->setFont(ArialMT_Plain_10);
-  display->setTextAlignment(TEXT_ALIGN_CENTER);
-  display->drawString(x + 64, y + 0, "RF Transmit");
-  display->drawXbm(x + 5, y + 18, arrows_width, arrows_height, arrows_bits);
-  display->setFont(ArialMT_Plain_16);
-  display->setTextAlignment(TEXT_ALIGN_LEFT);
-  display->drawString(x + 50, y + 25, rfTransmitterNode.getLastEvent());
 }
 
 void loop() { Homie.loop(); }
